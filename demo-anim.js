a1e4f8efff86ac793093c35ec61b259aad213010
1111hui <26634873@qq.com>
2016-07-31 16:44:07 +0800

    ADD: demo-anim files

---
+++ demo-anim.js
@@ -0,0 +1,30 @@
+// js for index.html
+
+var demo_animobj = {
+  '.box':{
+    $id: 'box',
+    float:'none'
+  }
+}
+
+var demo_animcss = cssobj(demo_animobj, {
+  local:{prefix:'demo-anim-'},
+  plugins:{
+    value:cssobj_plugin_value_default_unit()
+  },
+  onUpdate: updateDiff('demo_anim_diff')
+}, {left: 10})
+
+addPluginDisplayCSS(demo_animcss, 'demo_anim_text')
+
+function demo_anim_runner() {
+  var box = demo_animobj['.box']
+  var data = demo_animcss.data
+  data.left += data.dir || 1
+  if(data.left>500) data.dir=-1
+  if(data.left<0) data.dir=1
+  box.marginLeft = data.left
+  demo_animcss.update()
+}
+
+setInterval(demo_anim_runner, 4)
